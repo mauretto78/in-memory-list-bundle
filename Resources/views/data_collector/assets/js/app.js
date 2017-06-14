ca18ad14cbcfe4d009748ddd596f99f9b840714c
Mauro Cassani <m.cassani@bestnetwork.it>
2017-06-14 15:04:39 +0200

    codacy js style fix

--- Resources/views/data_collector/assets/js/app.js
+++ Resources/views/data_collector/assets/js/app.js
@@ -24,16 +24,16 @@ function removeElement(uuid) {
 
 // Show no data
 function showNoData() {
-    removeElement('inmemory_list_table');
-    removeElement('inmemory_list_flush_cache');
-    document.getElementById("inmemory_list_show").innerHTML = '<p>No data in cache</p>';
+    removeElement("inmemory_list_table");
+    removeElement("inmemory_list_flush_cache");
+    document.getElementById("inmemory_list_show").innerHTML = "<p>No data in cache</p>";
 }
 
 var xmlhttp = new XMLHttpRequest();
 
 // Flush entire cache
 document.getElementById("inmemory_list_flush_cache").onclick = function(e) {
-    if (confirm('Are you sure you want purge the entire cache?')) {
+    if (confirm("Are you sure you want purge the entire cache?")) {
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState === XMLHttpRequest.DONE ) {
                 if (xmlhttp.status === 204) {
@@ -53,8 +53,8 @@ document.getElementById("inmemory_list_flush_cache").onclick = function(e) {
 function deleteList() {
     var deleteElement = document.getElementsByClassName("inmemory_list_delete_element");
     for (var i = 0 ; i < deleteElement.length; i++) {
-        deleteElement[i].addEventListener('click' , function (e) {
-            if (confirm('Are you sure you want delete this list?')) {
+        deleteElement[i].addEventListener("click" , function (e) {
+            if (confirm("Are you sure you want delete this list?")) {
                 var uuid = this.getAttribute("data-id");
 
                 xmlhttp.onreadystatechange = function() {
@@ -62,7 +62,7 @@ function deleteList() {
                         if (xmlhttp.status === 204) {
                             removeElement(uuid);
 
-                            var rows = document.getElementById('inmemory_list_table').getElementsByTagName("tr").length;
+                            var rows = document.getElementById("inmemory_list_table").getElementsByTagName("tr").length;
                             if((rows -1) === 0){
                                 showNoData();
                             }
@@ -86,7 +86,7 @@ function decrTtl(ttl, element) {
             element.innerHTML = ttl--;
 
             if(ttl <= 0){
-                removeElement(element);
+                showList()
             }
         }, 1000);
     }
@@ -109,39 +109,41 @@ function showList() {
         if (xmlhttp.readyState === XMLHttpRequest.DONE ) {
             if (xmlhttp.status === 200) {
                 var list = JSON.parse(xmlhttp.responseText),
-                    response = '';
+                    response = "";
 
                 if(Object.size(list)>0) {
-                    response += '<table id="inmemory_list_table">';
-                    response += '<thead>';
-                    response += '<tr>';
-                    response += '<th class="key" scope="col">List</td>';
-                    response += '<th scope="col">Created on</td>';
-                    response += '<th scope="col">Expires on</td>';
-                    response += '<th scope="col">Ttl</td>';
-                    response += '<th scope="col">Items</td>';
-                    response += '<th scope="col">Chunks</td>';
-                    response += '<th scope="col">Chunk size</td>';
-                    response += '<th scope="col"></td>';
-                    response += '</tr>';
-                    response += '</thead>';
+                    response += "<table id='inmemory_list_table'>";
+                    response += "<thead>";
+                    response += "<tr>";
+                    response += "<th class='key' scope='col'>List</td>";
+                    response += "<th scope='col'>Created on</td>";
+                    response += "<th scope='col'>Expires on</td>";
+                    response += "<th scope='col'>Ttl</td>";
+                    response += "<th scope='col'>Items</td>";
+                    response += "<th scope='col'>Chunks</td>";
+                    response += "<th scope='col'>Chunk size</td>";
+                    response += "<th scope='col'></td>";
+                    response += "</tr>";
+                    response += "</thead>";
 
-                    for (var item in list) {
-                        response += '<tr id="'+list[item]['uuid']+'">';
-                        response += '<th class="font-normal">'+list[item]['uuid']+'</th>';
-                        response += '<td class="font-normal">'+list[item]['created_on']+'</td>';
-                        response += '<td class="font-normal">'+list[item]['expires_on']+'</td>';
-                        response += '<td class="font-normal ttl" data-ttl="'+list[item]['ttl']+'">'+list[item]['ttl']+'</td>';
-                        response += '<td class="font-normal">'+list[item]['size']+'</td>';
-                        response += '<td class="font-normal">'+list[item]['chunks']+'</td>';
-                        response += '<td class="font-normal">'+list[item]['chunk-size']+'</td>';
-                        response += '<td class="font-normal"><a href="#" class="inmemory_list_delete_element" data-id="'+list[item]['uuid']+'">Delete</a></td>';
-                        response += '</tr>';
+                    for (item in list) {
+                        if ({}.hasOwnProperty.call(list, item)) {
+                            response += "<tr id='"+list[item]["uuid"]+"'>";
+                            response += "<th class='font-normal'>"+list[item]["uuid"]+"</th>";
+                            response += "<td class='font-normal'>"+list[item]["created_on"]+"</td>";
+                            response += "<td class='font-normal'>"+list[item]["expires_on"]+"</td>";
+                            response += "<td class='font-normal ttl' data-ttl='"+list[item]["ttl"]+"'>"+list[item]["ttl"]+"</td>";
+                            response += "<td class='font-normal'>"+list[item]["size"]+"</td>";
+                            response += "<td class='font-normal'>"+list[item]["chunks"]+"</td>";
+                            response += "<td class='font-normal'>"+list[item]["chunk-size"]+"</td>";
+                            response += "<td class='font-normal'><a href='#' class='inmemory_list_delete_element' data-id='"+list[item]["uuid"]+"'>Delete</a></td>";
+                            response += "</tr>";
+                        }
                     }
 
-                    response += '</table>';
+                    response += "</table>";
                 } else {
-                    response += '<p>No data in cache</p>';
+                    response += "<p>No data in cache</p>";
                 }
 
                 // render content
